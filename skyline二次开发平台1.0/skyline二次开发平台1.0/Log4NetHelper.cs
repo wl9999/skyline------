64791b37b10859183e9f3a72e9a54868777ed859
wl9999 <40790080+wl9999@users.noreply.github.com>
2018-07-06 18:00:10 +0800

    skyline2.0
    文件打开，文字标签，显示经纬度

---
+++ skyline二次开发平台1.0/skyline二次开发平台1.0/Log4NetHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skyline二次开发平台1._0
+{
+    class Log4NetHelper
+    {
+        public static void WriteLog(Type e, Exception ex)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(e);
+            log.Error("Error", ex);
+        }
+
+        public static void WriteLog(Type e, string msg)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(e);
+            log.Error(msg);
+        }
+    }
+}
